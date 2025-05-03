97b0d324baeb2d0b9a113a0f1d3e859c475ceb2d
David Saldarriaga <david.salda98@gmail.com>
2025-05-02 20:56:10 -0500

    primer commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "! Welcome to Java programming.");
+        scanner.close();
+    }
+}
